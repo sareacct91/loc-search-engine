5f1a3eae4694bff13c7bbb08f3ff4ab36daf9af2
Meow Roar <sareacct91@gmail.com>
2023-12-04 17:37:48 -0800

    Added example of url

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ const searchTopicIndex = document.querySelector('#searchInput');
 const selectFormatIndex = document.querySelector("#formatSelect");
 const searchBtnIndex = document.querySelector('#searchBtn');
 
+// Example: https://www.loc.gov/film-and-videos/?q=dog&fo=json
 let queryURL = "https://www.loc.gov/{endpoint}/?fo=json"
-// above needs work
 
+// above needs work
