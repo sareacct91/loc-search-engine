e7a45236d6f62551615e793462bff3eba3a73f49
Meow Roar <sareacct91@gmail.com>
2023-12-05 10:49:40 -0800

    Added event handler for submit of the form to go to another .html file

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,10 @@
 const formInputEl = document.querySelector('#userSearchInput');
 const searchTopicEl = document.querySelector('#searchInput');
 const selectFormatEl = document.querySelector("#formatSelect");
+
+
+formInputEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  document.location.href = `/search-results.html?q=${searchTopicEl.value}&format=${selectFormatEl.value === 'Please select a format...' ? '' : selectFormatEl.value}`;
+})
