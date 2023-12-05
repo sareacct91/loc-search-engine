b9560dd9b69fdff84dfdae2f8b243303351562b9
Meow Roar <sareacct91@gmail.com>
2023-12-05 10:51:10 -0800

    Do an API call when the page load using the queryparam from index.html

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -6,3 +6,44 @@ const searchBtn = document.querySelector("#searchBtn");
 const backBtn = document.querySelector("#backBtn");
 const topicTxt = document.querySelector("#topicSpan");
 const searchResultsEl = document.querySelector('#searchResults');
+
+
+
+function displayResult(resultsArr) {
+  
+}
+
+function getAPIurl() {
+  /* 
+    /search-results.html?q=dog&format=photos
+  0  ?q=dog
+    &
+  1  format=photos
+  */
+  const queryParamArr = document.location.search.split('&');
+  const searchTopic = queryParamArr[0].split('=')[1];
+  const format = queryParamArr[1].split('=')[1] || 'search';
+
+  console.log(searchTopic);
+  console.log(format);
+
+  return `https://www.loc.gov/${format}/?q=${searchTopic}&fo=json`;
+}
+
+function fetchAPIdata(url) {
+  fetch(url).then(response => response.json()).then(data => {
+    console.log(data.results);
+    displayResult(data.results);
+  })
+}
+
+
+
+
+
+
+
+// Init IIFE
+(onDOMContentLoaded = () => {
+  fetchAPIdata(getAPIurl());
+})();
