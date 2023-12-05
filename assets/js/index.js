3ff6fe1dffd2889d8f368534e043141c0da0880a
Meow Roar <sareacct91@gmail.com>
2023-12-05 13:44:44 -0800

    Update the link to 2nd html

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,5 +7,5 @@ const selectFormatEl = document.querySelector("#formatSelect");
 formInputEl.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  document.location.href = `/search-results.html?q=${searchTopicEl.value}&format=${selectFormatEl.value === 'Please select a format...' ? '' : selectFormatEl.value}`;
+  document.location.href = `./search-results.html?q=${searchTopicEl.value}&format=${selectFormatEl.value === 'Please select a format...' ? '' : selectFormatEl.value}`;
 })
