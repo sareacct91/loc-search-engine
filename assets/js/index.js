3ef084aeaf80672aa3231d5c04780ca0878332bd
Meow Roar <sareacct91@gmail.com>
2023-12-04 17:21:14 -0800

    Changed elements ids

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,7 @@
-// Because of similar elements on index and search-results pages -- ids have been specified using either 'index' or 'search'
-let searchBtnIndex = document.getElementById('searchBtnIndex');
-let searchBtnSearch = document.getElementById('searchBtnSearch');
-let backBtn = document.getElementById('backBtn');
-let topicTxt = document.getElementById('topicSpan');
-let searchTopicIndex = document.getElementById('searchTxtIndex');
-let searchTopicSearch = document.getElementById('searchTxtSearch');
-let selectFormatIndex = document.getElementById('formatSelectIndex');
-let selectFormatSearch = document.getElementById('formatSelectSearch');
-let formatType; // this should equal the value chosen by user in the select input
-
+// DOM selectors
+const searchTopicIndex = document.querySelector('#searchInput');
+const selectFormatIndex = document.querySelector("#formatSelect");
+const searchBtnIndex = document.querySelector('#searchBtn');
 
 let queryURL = "https://www.loc.gov/{endpoint}/?fo=json"
 // above needs work
