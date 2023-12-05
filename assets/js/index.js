e3ed291d2bf48c6718f8e51fbcebc32f5c848f93
Meow Roar <sareacct91@gmail.com>
2023-12-05 14:20:47 -0800

    Bug fix: trim user input

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,5 +7,6 @@ const selectFormatEl = document.querySelector("#formatSelect");
 formInputEl.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  document.location.href = `./search-results.html?q=${searchTopicEl.value}&format=${selectFormatEl.value === 'Please select a format...' ? '' : selectFormatEl.value}`;
+  document.location.href =
+    `./search-results.html?q=${searchTopicEl.value.trim()}&format=${selectFormatEl.value === 'Please select a format...' ? '' : selectFormatEl.value}`;
 })
