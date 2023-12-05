c8d699e053615da0b5c590e293ea3a0161c7d4d1
Meow Roar <sareacct91@gmail.com>
2023-12-04 17:21:29 -0800

    Removed unuse DOM selector

---
+++ assets/js/search-results.js
@@ -0,0 +1,5 @@
+const searchBtnSearch = document.getElementById("searchBtnSearch");
+const backBtn = document.getElementById("backBtn");
+const topicTxt = document.getElementById("topicSpan");
+const searchTopicSearch = document.getElementById("searchTxtSearch");
+const selectFormatSearch = document.getElementById("formatSelectSearch");
