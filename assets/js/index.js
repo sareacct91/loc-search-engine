11906b30e680d2b029a4cc6e8f75fdc72da530db
Meow Roar <sareacct91@gmail.com>
2023-12-05 09:35:10 -0800

    Re-initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,4 @@
 // DOM selectors
-const searchTopicIndex = document.querySelector('#searchInput');
-const selectFormatIndex = document.querySelector("#formatSelect");
-const searchBtnIndex = document.querySelector('#searchBtn');
-
-// Example: https://www.loc.gov/film-and-videos/?q=dog&fo=json
-let queryURL = "https://www.loc.gov/{endpoint}/?fo=json"
-
-// above needs work
+const formInputEl = document.querySelector('#userSearchInput');
+const searchTopicEl = document.querySelector('#searchInput');
+const selectFormatEl = document.querySelector("#formatSelect");
--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,5 +1,8 @@
-const searchTopicSearch = document.querySelector("#searchInput");
-const selectFormatSearch = document.querySelector("#formatSelect");
-const searchBtnSearch = document.querySelector("#searchBtn");
+// DOM selectors
+const formInputEl = document.querySelector('#userSearchInput');
+const searchTopicEl = document.querySelector('#searchInput');
+const selectFormatEl = document.querySelector("#formatSelect");
+const searchBtn = document.querySelector("#searchBtn");
 const backBtn = document.querySelector("#backBtn");
 const topicTxt = document.querySelector("#topicSpan");
+const searchResultsEl = document.querySelector('#searchResults');
